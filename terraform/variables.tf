04959d9d87062a3f941f8e83c81f87cd57fb9a88
Patricia Szasz <patricia.szasz@endava.com>
2017-07-19 15:20:40 +0000

    description and comment

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,8 @@
+#Variables file
+
 variable "awsregion" {
   description = "AWS region used to create resources"
   type = "string"
+  #default value is optional but if no default provided, the variable is considered required and terraform will error
   default = "us-east-1"
 }
