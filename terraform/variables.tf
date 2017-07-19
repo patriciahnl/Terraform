3d713e7aa44786a3825f47b051d65e962c3dea57
Patricia Szasz <patricia.szasz@endava.com>
2017-07-19 17:53:01 +0000

    declare variables for vpc config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,43 @@
-#Variables file
+#Variables file specifying variable types and default values
+#Check conf.tfvars (this will will be automatically loaded unless other file is specified)
+
+
 
 variable "awsregion" {
   description = "AWS region used to create resources"
   type = "string"
   #default value is optional but if no default provided, the variable is considered required and terraform will error
   default = "us-east-1"
+}
+
+
+#VPC variables
+
+variable "cidr" {
+  description = "VPC CIDR block"
+  type = "string"
+}
+
+variable "enable_dns_support" {
+  description = "Should be true if you want DNS resolution supported by VPC. Default value for user created VPCs is true."
+  default = true
+}
+
+variable "enable_dns_hostnames" {
+  description = "True means the instances launched in that VPC will get a public DNS hostname" 
+  default = false
+}
+
+variable "vpc_resource_tags" {
+  description = "Map of tags for all instances launched inside the created VPC"
+  type = "map"
+  default = {
+    "Scope" = "LicentaPatri"
+    "Provisioner" = "terraform"
+  }
+}
+
+variable "vpc_name" {
+  description = "Name tag for created VPC"
+  type = "string"
 }
