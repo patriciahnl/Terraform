0ad5f1b5821cff1ca3d211d4914492c577ae6748
Patricia Szasz <patricia.szasz@endava.com>
2017-07-20 15:51:28 +0000

    add private subnet configuration

--- terraform/vpcconfig.tf
+++ terraform/vpcconfig.tf
@@ -25,10 +25,10 @@ resource "aws_internet_gateway" "igwpublicsubnet" {
 
 #Public subnet resource
 resource "aws_subnet" "publicsubnet" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = "${aws_vpc.mainvpc.id}"
   cidr_block = "${var.public_subnet_cidr_block}"
   availability_zone = "${var.subnet_avz}"
-  tags = "${merge(var.vpc_resource_tags, map("VPC", var.vpc_name), map("AvailabilityZone", var.))}"
+  tags = "${merge(var.vpc_resource_tags, map("VPC", var.vpc_name), map("AvailabilityZone", var.subnet_avz))}"
   lifecycle {
     create_before_destroy = true
   }
@@ -45,12 +45,46 @@ resource "aws_route_table" "publicroute" {
   }
   
   tags = "${merge(var.vpc_resource_tags, map("VPC", var.vpc_name), map("Name", "Public Subnet Route"))}"
+  lifecycle { create_before_destroy = true }
+
 }
 
 #Associate public route table to public subnet
 resource "aws_route_table_association" "publicrouteassoc" {
   subnet_id = "${aws_subnet.publicsubnet.id}"
   route_table_id = "${aws_route_table.publicroute.id}"
+}
+
+#PRIVATE SUBNET CONFIGURATION
+#Private subnet
+resource "aws_subnet" "privatesubnet" {
+  vpc_id = "${aws_vpc.mainvpc.id}"
+  cidr_block = "${var.private_subnet_cidr_block}"
+  availability_zone = "${var.subnet_avz}"
+  tags = "${merge(var.vpc_resource_tags, map("VPC", var.vpc_name), map("AvailabilityZone", var.subnet_avz))}"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#Route table for private subnet. Route towards 0.0.0.0/0 through instance performing NAT
+
+resource "aws_route_table" "privateroute" {
+  vpc_id = "${aws_vpc.mainvpc.id}"
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    instance_id = "${module.nat.instance_id}"
+  }
+  
+  tags = "${merge(var.vpc_resource_tags, map("VPC", var.vpc_name), map("Name", "Private Subnet Route"))}"
+  lifecycle { create_before_destroy = true }
+}
+#Associate route to private subnet
+resource "aws_route_table_association" "privaterouteassoc" {
+  subnet_id = "${aws_subnet.privatesubnet.id}"
+  route_table_id = "${aws_route_table.privateroute.id}"
+  lifecycle { create_before_destroy = true }
 }
 
 
