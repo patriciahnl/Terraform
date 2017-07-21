d7e14dfad61d7b587f94a6647ef4ccb002c6ab7e
Patricia Szasz <patricia.szasz@endava.com>
2017-07-21 00:27:27 +0000

    elastic ip allocation config

---
+++ aws-setup/modules/nat/eip_alloc.tf
@@ -0,0 +1,12 @@
+#Elastic IP allocation for NAT instance
+
+resource "aws_eip" "nat_instance_eip" {
+  instance = "${aws_instance.nat_instance.id}"
+  vpc = true
+}
+
+#Allocate
+resource "aws_eip_allocation" "eip_alloc_nat" {
+  instance_id = "${aws_instance.nat_instance.id}"
+  allocation_id = "${aws_eip.nat_instance_eip.id}"
+}
